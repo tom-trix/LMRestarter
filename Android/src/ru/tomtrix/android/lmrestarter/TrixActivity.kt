09e0ebf827d5d90a5b28f606edfbf92f588a6574
tom-trix_nbw <tom-trix@ya.ru>
2013-09-05 00:28:29 +0600

    Вроде всё сделал. Остался Debug и стилистика

--- Android/src/ru/tomtrix/android/lmrestarter/TrixActivity.kt
+++ Android/src/ru/tomtrix/android/lmrestarter/TrixActivity.kt
@@ -3,23 +3,37 @@ package ru.tomtrix.android.lmrestarter
 import java.io.*
 import java.net.*
 import android.os.*
+import android.app.*
 import android.widget.*
 import android.util.Log
 import android.view.View
-import android.app.Activity
+import android.graphics.Color
 import android.os.AsyncTask.Status
+import android.content.DialogInterface
 
 public open class TrixActivity() : Activity() {
 
-    var _password = ""
-    var handler: Handler? = null
+    /** Password for authentication */
+    private var _password = ""
+    /** Current server */
+    private var _server = "lordmancer.ru"
+    /** Handler to process GUI-operations inside a non-GUI thread */
+    private var _handler: Handler? = null
 
+
+
+    /** Entry point */
     public override fun onCreate(savedInstanceState: Bundle?): Unit {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        handler = Handler()
+        _handler = Handler()
     }
 
+    /**
+     * Performs the request to Node.js server
+     * @param url - server's URL
+     * @param password - password
+     */
     private fun doRequest(url: String, password: String): String {
         return object : AsyncTask<String, Void, String>() {
             protected override fun doInBackground(vararg p0: String?): String? {
@@ -34,35 +48,51 @@ public open class TrixActivity() : Activity() {
         }.execute(url, password).get() ?: "Error"
     }
 
+    /** Shows a simple message */
     private fun showMsg(s: String?) {
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
     }
 
-    public fun btnAuthOnClick(v: View) {
+    /** Shows a YES-NO dialog and runs the <b>onYes</b> function if the user clicks YES */
+    private fun showDialog(s: String, onYes: () -> Unit) {
+        var builder = AlertDialog.Builder(this)
+        builder.setMessage(s)
+        builder.setNegativeButton("No", null)
+        builder.setPositiveButton("Yes", object : DialogInterface.OnClickListener {
+            public override fun onClick(p0: DialogInterface, p1: Int) {
+                onYes()
+            }
+        })
+        builder.create().show()
+    }
+
+
+
+    public fun btnAuthClick(v: View) {
         val txtPswd = findViewById(R.id.txtPswd) as EditText
         val activity = this
 
         if (txtPswd.getVisibility() == View.INVISIBLE) {
-            ///val s = doRequest("http://lordmancer.ru:20008/register", "")
-            val s = doRequest("http://lordmancer.ru:20008/isrun", "")
+            val s = doRequest("http://$_server:20008/register", "")
             if (s.contains("Exception")) {showMsg("No connection to Node.js server"); return}
             txtPswd.setVisibility(View.VISIBLE)
             (v as Button).setText("Come on!")
         } else {
-            ///_password = txtPswd.getText().toString()
-            _password = "214976"
-            if (doRequest("http://lordmancer.ru:20008/isrun", _password).startsWith("Not auth")) {showMsg("Wrong password!"); return}
+            _password = txtPswd.getText().toString()
+            if (doRequest("http://$_server:20008/isrun", _password).startsWith("Not auth")) {showMsg("Wrong password!"); return}
             setContentView(R.layout.second)
             Thread(object : Runnable {
                 public override fun run() {
                     while (!activity.isFinishing()) {
-                        var s = doRequest("http://lordmancer.ru:20008/isrun", _password);
+                        var s = doRequest("http://$_server:20008/isrun", _password);
                         if (s.startsWith("Not auth")) {showMsg("Your password is expired! Sign in again!"); return}
                         val isrun = java.lang.Boolean.parseBoolean(s)
-                        Log.d("Trix", "isrun = " + isrun)
-                        handler?.post(object : Runnable {
+                        _handler?.post(object : Runnable {
                             public override fun run() {
-                                (activity.findViewById(R.id.btnHey) as Button).setText(if (isrun) "Server is running" else "Server is stopped")
+                                val lblStatus = activity.findViewById(R.id.lblStatus) as TextView
+                                lblStatus.setText(if (isrun) "Server is running" else "Server is stopped")
+                                lblStatus.setTextColor(if (isrun) Color.GREEN else Color.rgb(220, 50, 50))
+                                (activity.findViewById(R.id.btnStart) as Button).setEnabled(!isrun)
                             }
                         })
                         Thread.sleep(6000)
@@ -70,5 +100,31 @@ public open class TrixActivity() : Activity() {
                 }
             }).start()
         }
+    }
+
+    public fun btnStopClick(v: View) {
+        val s = doRequest("http://$_server:20008/stop", _password)
+        showMsg(if (s.startsWith("Not auth")) "Your password is expired" else "Server sent: $s")
+    }
+
+    public fun btnKillClick(v: View) {
+        val s = doRequest("http://$_server:20008/kill", _password)
+        showMsg(if (s.startsWith("Not auth")) "Your password is expired" else "Server sent: $s")
+    }
+
+    public fun btnWipeoutClick(v: View) {
+        showDialog("This action is NOT recommended! Continue?", {() ->
+            val s = doRequest("http://$_server:20008/wipeout", _password)
+            showMsg(if (s.startsWith("Not auth")) "Your password is expired" else "Server sent: $s")
+        })
+    }
+
+    public fun btnStartClick(v: View) {
+        val s = doRequest("http://$_server:20008/start", _password)
+        showMsg(if (s.startsWith("Not auth")) "Your password is expired" else "Server sent: $s")
+    }
+
+    public fun radioServerIndexChanged(v: View) {
+        _server = (v as RadioButton).getText().toString()
     }
 }
